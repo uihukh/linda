f447370d13f763d1ac2891ff402ad5a0561b2d3a
uihukh <44710846+uihukh@users.noreply.github.com>
2018-11-06 14:28:18 +0700

    Update index.js

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const http = require('http');
 (async () => { 
   const requestHandler = (request, response) => {  
        console.log(request.url)
-       response.end('Hello World')
+       response.end('Hello Worldc')
   }
        
   const server = http.createServer(requestHandler)
@@ -27,10 +27,11 @@ function kiusi(){
   const browser = await puppeteer.launch({args: ['--no-sandbox', '--disable-setuid-sandbox']});
   
   const page = await browser.newPage();
-  await page.goto('https://uihukh.github.io/dutangle/index.html'); 
+  // await page.goto('https://uihukh.github.io/dutangle/index.html'); 
+  await page.goto('https://eu0.proxysite.com/process.php?d=fOAq6knT%2Btu3Z6CvE6zbeKClgTs6Wosx%2BS%2FHO9OvRKeQeSABYvJvzY210Kv290DFK4%2Fm&b=1&f=norefer');    
   console.log('----XONG0--------');
-  setTimeout(async () => await await page.close(), 185000);
-  setTimeout(async () => await browser.close(), 193000);
+  setTimeout(async () => await await page.close(), 50000);
+  setTimeout(async () => await browser.close(), 60000);
   console.log('----XONG--------');
   
   })();
@@ -40,7 +41,7 @@ function f() {
     console.log('-111111111111-------trong f -----------');
     kiusi();
     console.log(Date()); 
-    setTimeout( f, 200000 ); 
+    setTimeout( f, 70000 ); 
     console.log('--tttttttttt--') 
 }
 
